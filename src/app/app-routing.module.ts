8ef8ad66afa6b8f3f3c4c691704079061e68cb20
saba0308 <sabapathi030820@gmail.com>
2022-08-27 15:12:30 +0530

    5 files modified, 9 added, 3 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BusComponent } from './views/bus/bus/bus.component';
+import { HomeComponent } from './views/home/home.component';
+import { TestimonialsComponent } from './views/testimonials/testimonials.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  declarations: [ 
+    BusComponent, HomeComponent, TestimonialsComponent
+  ],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'app-bus', component: BusComponent },
+      { path: 'app-home', component: HomeComponent },
+      { path: 'app-testimonials', component: TestimonialsComponent},
+      { path: '**', redirectTo: 'app-home' }
+    ])
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: [],
+
 })
 export class AppRoutingModule {}
+
+
