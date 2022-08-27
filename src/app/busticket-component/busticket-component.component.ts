6b2803cf1e9d4502c66defad59bd7c4004237463
saba0308 <sabapathi030820@gmail.com>
2022-08-27 12:32:06 +0530

    1 file modified, 4 added, 1 deleted

---
+++ src/app/busticket-component/busticket-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busticket-component',
+  templateUrl: './busticket-component.component.html',
+  styleUrls: ['./busticket-component.component.css']
+})
+export class BusticketComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
